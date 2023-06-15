6f9e392b9a606fa6f82fbed983956eb3824d7b54
bikraj2 <bikrajshrestha2@gmail.com>
2023-06-15 14:58:34 +0545

    Testing Smart Contract in HardHat

--- BlockChain/HardHat101/scripts/company.ts
+++ BlockChain/HardHat101/scripts/company.ts
@@ -1,27 +1,27 @@
-import { Address } from './../node_modules/ethereumjs-util/src/address';
+// import { Address } from './../node_modules/ethereumjs-util/src/address';
 
 
-const { ethers } = require('ethers');
-require('dotenv').config();
-var keepRunning: boolean = true;
-async function main() {
-  const url = process.env.RPC_URL;
-  let artifacts = await hre.artifacts.readArtifact('Company');
-  const provider = new ethers.JsonRpcProvider(url);
-  const privateKey = process.env.TESTNET_PRIVATE_KEY;
-  let wallet = new ethers.Wallet(privateKey!, provider);
-  let ModifyVariable = new ethers.ContractFactory(
-    artifacts.abi,
-    artifacts.bytecode,
-    wallet
-  );
+// const { ethers } = require('ethers');
+// require('dotenv').config();
+// var keepRunning: boolean = true;
+// async function main() {
+//   const url = process.env.RPC_URL;
+//   let artifacts = await hre.artifacts.readArtifact('Company');
+//   const provider = new ethers.JsonRpcProvider(url);
+//   const privateKey = process.env.TESTNET_PRIVATE_KEY;
+//   let wallet = new ethers.Wallet(privateKey!, provider);
+//   let ModifyVariable = new ethers.ContractFactory(
+//     artifacts.abi,
+//     artifacts.bytecode,
+//     wallet
+//   );
 
-  // we then use the ContractFactory object to deploy an instance of the contract
-  const contract = await ModifyVariable.deploy({ gasLimit: 1000000 });
-  const address: Address = await contract.owner();
-  console.log(address);
-}
+//   // we then use the ContractFactory object to deploy an instance of the contract
+//   const contract = await ModifyVariable.deploy({ gasLimit: 1000000 });
+//   const address: Address = await contract.owner();
+//   console.log(address);
+// }
 
-main()
-  .then(() => console.log('Completed Interaction'))
-  .catch((error) => console.log(error));
+// main()
+//   .then(() => console.log('Completed Interaction'))
+//   .catch((error) => console.log(error));
--- BlockChain/HardHat101/scripts/deploy.ts
+++ BlockChain/HardHat101/scripts/deploy.ts
@@ -3,7 +3,7 @@ require('dotenv').config();
 var keepRunning: boolean = true;
 async function main() {
   const url = process.env.RPC_URL;
-  let artifacts = await hre.artifacts.readArtifact('Counter');
+  let artifacts = await hre.artifacts.readArtifact('Company1');
   const provider = new ethers.JsonRpcProvider(url);
   const privateKey = process.env.TESTNET_PRIVATE_KEY;
   let wallet = new ethers.Wallet(privateKey!, provider);
@@ -12,10 +12,10 @@ async function main() {
     artifacts.bytecode,
     wallet
   );
-
   // we then use the ContractFactory object to deploy an instance of the contract
- 
-  const contract = await Company.deploy();
+    const contract = await Company.deploy(['overrides']);
+
+    console.log(await contract.getSalary("b"))
 }
 
 main()
